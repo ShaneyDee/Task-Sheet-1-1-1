9e29911407d83af5b64875f4e1d05e20d3ce19a5
Shane Deocales <126802691+ShaneyDee@users.noreply.github.com>
2023-12-22 21:16:46 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
-class Main {
+
+//change class name "HelloWorldApp.java" to "Main.java".
+class Main { 
     public static void main(String[] args) {
+        //change "Hello World!" output to "Kamusta Mundo!".
         System.out.println("Kamusta Mundo!"); 
     }
 }
